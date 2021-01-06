938dddf9e6e71f5580271baddb9aba8372d93d44
Sugato Ray <sugatoray@users.noreply.github.com>
2021-01-05 22:15:08 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,18 +12,18 @@ import os
 PKG = "colab_git_ssh"
 VERSIONFILE = os.path.join(PKG, "_version.py")
 verstr = "unknown"
-try:
-    verstrline = open(VERSIONFILE, "rt").read()
-except EnvironmentError:
-    pass # Okay, there is no version file.
-else:
-    VSRE = r"^verstr = ['\"]([^'\"]*)['\"]"
-    mo = re.search(VSRE, verstrline, re.M)
-    if mo:
-        verstr = mo.group(1)
-    else:
-        print "unable to find version in %s" % (VERSIONFILE,)
-        raise RuntimeError("if %s.py exists, it is required to be well-formed" % (VERSIONFILE,))
+# try:
+#     verstrline = open(VERSIONFILE, "rt").read()
+# except EnvironmentError:
+#     pass # Okay, there is no version file.
+# else:
+#     VSRE = r"^verstr = ['\"]([^'\"]*)['\"]"
+#     mo = re.search(VSRE, verstrline, re.M)
+#     if mo:
+#         verstr = mo.group(1)
+#     else:
+#         print "unable to find version in %s" % (VERSIONFILE,)
+#         raise RuntimeError("if %s.py exists, it is required to be well-formed" % (VERSIONFILE,))
 
 ###--------------------------------------------------###
                
